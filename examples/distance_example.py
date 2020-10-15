5f71a55f8e9c0914de74971dc69a5ac047477874
Guillaume Florent <guillaume.florent@tutanota.com>
2020-10-15 13:09:38 +0200

    Improved examples

--- examples/distance_example.py
+++ examples/distance_example.py
@@ -6,7 +6,7 @@ r"""Example distance conversion."""
 from ydeos_units.units import cm, inches
 
 # convert 12.0 inches to centimeters
-print(cm(inches=12.0))
+print(f"12 inches is {cm(inches=12.0):.6f} centimetres")
 
 # convert 55 cm to inches
-print(inches(cm=55))
+print(f"55 centimetres is {inches(cm=55):.6f} inches")
--- examples/imperial_units_example.py
+++ examples/imperial_units_example.py
@@ -5,24 +5,34 @@ r"""Imperial conversions."""
 
 from ydeos_units.units import convert_imperial, convert_imperial_str
 
+
+def print_convert_imperial(feet, to_unit, inches=0):
+    print(f"{feet}'{inches}'' is {convert_imperial(feet=feet, inches=inches, to_unit='mm')} {to_unit}")
+
+
+def print_convert_imperial_str(value, to_unit):
+    v_mm = convert_imperial_str(value=value, to_unit=to_unit)
+    print(f"{value} is {v_mm} mm")
+
+
 if __name__ == "__main__":
-    print(convert_imperial(feet=1., inches=2.5, to_unit="mm"))
-    print(convert_imperial_str(value="1'2''1/2", to_unit="mm"))
+    print_convert_imperial(feet=1., inches=2.5, to_unit='mm')
+    print_convert_imperial_str(value="1'2''1/2", to_unit='mm')
 
-    print(convert_imperial(feet=1., inches=2, to_unit="mm"))
-    print(convert_imperial_str(value="1'2''", to_unit="mm"))
+    print_convert_imperial(feet=1., inches=2, to_unit="mm")
+    print_convert_imperial_str(value="1'2''", to_unit="mm")
 
-    print(convert_imperial(feet=1., inches=2.625, to_unit="mm"))
-    print(convert_imperial_str(value="1'2''5/8", to_unit="mm"))
+    print_convert_imperial(feet=1., inches=2.625, to_unit="mm")
+    print_convert_imperial_str(value="1'2''5/8", to_unit="mm")
 
-    print(convert_imperial(feet=0., inches=1., to_unit="mm"))
-    print(convert_imperial_str(value="1''", to_unit="mm"))
+    print_convert_imperial(feet=0., inches=1., to_unit="mm")
+    print_convert_imperial_str(value="1''", to_unit="mm")
 
-    print(convert_imperial(feet=0., inches=1.5, to_unit="mm"))
-    print(convert_imperial_str(value="1''1/2", to_unit="mm"))
+    print_convert_imperial(feet=0., inches=1.5, to_unit="mm")
+    print_convert_imperial_str(value="1''1/2", to_unit="mm")
 
-    print(convert_imperial(feet=8., to_unit="mm"))
-    print(convert_imperial_str(value="8'", to_unit="mm"))
+    print_convert_imperial(feet=8., to_unit="mm")
+    print_convert_imperial_str(value="8'", to_unit="mm")
 
-    print(convert_imperial(feet=8.5, to_unit="mm"))
-    print(convert_imperial_str(value="8.5'", to_unit="mm"))
+    print_convert_imperial(feet=8.5, to_unit="mm")
+    print_convert_imperial_str(value="8.5'", to_unit="mm")
